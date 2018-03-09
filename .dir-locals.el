663c7a4bb6fa1cc15ea37f8079f4bd7c1e26662f
Alex Branham <branham@utexas.edu>
2018-03-09 16:59:54 -0600

    Add bug-reference info to .dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
+         (bug-reference-url-format . "https://github.com/jabranham/helm-pass/issues/%s"))))
